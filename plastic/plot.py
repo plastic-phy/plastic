d44dab2bf1e1fce7a83a334c14dfa2af558e164d
Simone Ciccolella <s.ciccolella@campus.unimib.it>
2021-06-29 12:14:55 +0200

    updates to plot and notebooks

--- plastic/plot.py
+++ plastic/plot.py
@@ -1,5 +1,6 @@
 
 from ._core.genotypematrix import GenotypeMatrix
+from ._core.phylogenytree import PhylogenyTree
 from matplotlib import pyplot as plt
 
 def _build_colormap(unclustered, clustered):
@@ -30,3 +31,50 @@ def clusters(unclustered, clustered, ax=plt, **kwargs):
     red = pca.fit_transform(unclustered.matrix().transpose())
 
     ax.scatter(x=red[:,0], y=red[:,1], c=_build_colormap(unclustered.mutation_labels, clustered.mutation_labels), **kwargs)
+
+
+def _get_label_to_id_map(tree):
+    return {
+        node: '' if 'label' not in tree.nodes[node] else tree.nodes[node]['label']
+        for node in tree
+    }
+
+
+def phylogeny(tree, show_labels=False, show_support=False, graphviz_positioning=True, **kwargs):
+    import networkx as nx
+    from colour import Color
+
+    if not isinstance(tree, PhylogenyTree):
+        raise TypeError(
+            "tree needs to be a valid PhylogenyTree."
+        )
+    
+    c_gradient = list(Color("#3270FC").range_to(Color("#397D02"), 101))
+
+    if show_labels:
+        labels=_get_label_to_id_map(tree)
+    else:
+        labels=None
+    
+    if show_support:
+        colors = [c_gradient[int(v)].hex for k, v in nx.get_node_attributes(tree, 'support').items()]
+    else:
+        colors = None
+
+    if graphviz_positioning:
+        pos  = nx.nx_agraph.graphviz_layout(tree, prog="dot")
+    else:
+        pos = None
+
+    nx.draw(
+        tree,
+        pos=pos,
+        labels=labels,
+        node_color=colors,
+        **kwargs
+    )
+
+def similarity_matrix(matrix, **kwargs):
+    import seaborn as sns
+
+    sns.heatmap(matrix, **kwargs)
